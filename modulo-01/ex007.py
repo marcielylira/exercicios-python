a3150669652de8c0ecea8dffe1aa4e2de2ffddbf
Marciely <marcielylira@gmail.com>
2022-12-30 20:00:42 -0300

    Média aritmética

---
+++ modulo-01/ex007.py
@@ -0,0 +1,7 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+nota1 = float(input('Primeira nota: '))
+nota2 = float(input('Segunda nota: '))
+media = (nota1 + nota2) / 2
+print('Média do aluno:', media)
+
