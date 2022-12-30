253f205b42fb8cc258887d238209fb3013211de5
Marciely <marcielylira@gmail.com>
2022-12-30 12:07:27 -0300

    Antecessor e sucessor

---
+++ modulo-01/ex005.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+num= int(input('Digite um número: '))
+sucessor= num + 1
+antecessor= num - 1
+print('O número {} tem o sucessor {} e o antecessor {}'.format(num,sucessor,antecessor))
