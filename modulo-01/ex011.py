73302a986a0e0070889dc6b5f11f9920c7f5b923
Marciely <marcielylira@gmail.com>
2022-12-31 11:47:54 -0300

    Pintando uma parede

---
+++ modulo-01/ex011.py
@@ -0,0 +1,7 @@
+#Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.
+
+altura = float(input('Digite a alturada parede: '))
+largura = float(input('Digite a largura da parede: '))
+area = altura * largura
+tinta = area / 2
+print('A parede possui uma área de {}m² e será necessário {} litros de tinta'.format(area,tinta))
