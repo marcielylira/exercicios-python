c4a5c4c28d114c68225ed3bc4ee0fee307de9835
Marciely <marcielylira@gmail.com>
2023-01-06 12:06:31 -0300

    Analisando triângulo v1.0

---
+++ modulo-03/ex035.py
@@ -0,0 +1,9 @@
+#Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
+
+med1 = float(input('Digite a primeira medida: '))
+med2 = float(input('Digite a segunda medida: '))
+med3 = float(input('Digite a terceira medida: '))
+if med1 < med2 + med3 and med2 < med1 + med3 and med3 < med1 + med2:
+    print('É um triangulo')
+else:
+    print('Não é um triangulo')
