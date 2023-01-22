cc48b74b81f767101aaee389397651f553dcb6d0
Marciely <marcielylira@gmail.com>
2023-01-21 22:23:43 -0300

    Jogo do par ou ímpar

---
+++ modulo-06/ex068.py
@@ -0,0 +1,33 @@
+''' Faça um programa que jogue par ou ímpar com o computador.
+O jogo só será interrompido quando o jogador perder,
+mostrando o total de vitórias consecutivas que ele conquistou no final do jogo. '''
+
+from random import randint
+v = 0
+while True:
+    jogador = int(input('Digite um valor: '))
+    computador = randint(0, 10)
+    total = jogador + computador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('PAR ou IMPAR: ')).strip().upper()[0]
+    print(f'Você jogou {jogador} e o computador {computador}. Total de {total}')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você VENCEU!')
+            v += 1
+        else:
+            print('Você PERDEU!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você VENCEU!')
+            v += 1
+        else:
+            print('Você PERDEU!')
+            break
+    print(""
+          "")
+    print('-=-'*10)
+    print('Vamos jogar novamente...')
+print(f'\n\033[31mGAME OVER\033[m! Você venceu {v} vezes.')
